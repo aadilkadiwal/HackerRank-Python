f424ee8029acfaf64eec19ea9c864ccc6c44c0d9
aadilkadiwal <aadilkadiwal4277@gmail.com>
2022-03-25 19:34:57 +0530

    String Split and Join

---
+++ SplitJoin.py
@@ -0,0 +1,12 @@
+# String Split and Join
+
+def split_and_join(line):
+    split = line.split(" ")
+    join = "-".join(split)
+    return join
+
+'''    
+line = input()
+result = split_and_join(line)
+print(result)
+'''
