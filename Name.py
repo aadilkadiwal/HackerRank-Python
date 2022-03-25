26925bb9ae072253f13f5e1356cccb9afbc266cb
aadilkadiwal <aadilkadiwal4277@gmail.com>
2022-03-25 19:48:00 +0530

    What's Your Name?

---
+++ Name.py
@@ -0,0 +1,10 @@
+# What's Your Name?
+
+def print_full_name(first_name, last_name):
+    return print('Hello {} {}! You just delved into python.'.format(first_name, last_name))
+
+'''
+first_name = input()
+last_name = input()
+print_full_name(first_name, last_name)
+'''
