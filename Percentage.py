8e1c56451e420661622034994bec4228df0ed4f9
aadilkadiwal <aadilkadiwal4277@gmail.com>
2022-03-25 18:25:44 +0530

    Finding the Percentage

---
+++ Percentage.py
@@ -0,0 +1,12 @@
+# Finding the Percentage
+
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
+marks = student_marks[query_name]
+average = sum(marks)/len(marks)
+print(format(average, '.2f'))
