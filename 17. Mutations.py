e1d1d569bcf23baf2e77d90f612e6340953d68dc
aadilkadiwal <aadilkadiwal4277@gmail.com>
2022-03-26 11:37:33 +0530

    Mutations

---
+++ 17. Mutations.py
@@ -0,0 +1,14 @@
+# Mutations
+
+def mutate_string(string, position, character):
+    list_string = list(string)
+    list_string[position] = character
+    string = ''.join(list_string)
+    return string
+
+'''
+s = input()
+i, c = input().split()
+s_new = mutate_string(s, int(i), c)
+print(s_new)
+'''
